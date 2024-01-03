708a7424ec282fd19fd4d915e61e8773d5160054
lpofredc <frederic.cloitre@lpo.fr>
2024-01-03 14:10:26 +0100

    Add pictogram to sensitivy rules admin list

--- geotrek/sensitivity/admin.py
+++ geotrek/sensitivity/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
 from geotrek.common.mixins.actions import MergeActionMixin
 from geotrek.sensitivity.models import Rule, SportPractice, Species
@@ -6,8 +7,18 @@ from geotrek.sensitivity.models import Rule, SportPractice, Species
 
 class RuleAdmin(MergeActionMixin, admin.ModelAdmin):
     merge_field = "name"
-    list_display = ('name', 'code', )
+    list_display = ('name', 'code', 'thumb')
     search_fields = ('name', 'code', )
+
+    def thumb(self, obj):
+        return format_html(
+            f'''<a href="{obj.pictogram.url}" target="_blank">
+                  <img
+                    src="{obj.pictogram.url}" alt="{obj.pictogram.url}"
+                    width="30" height="30"
+                    style="object-fit: cover;"
+                  />
+                </a>''')
 
 
 class SportPracticeAdmin(MergeActionMixin, admin.ModelAdmin):
